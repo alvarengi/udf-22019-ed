581bda97b1d48c07b6f680745a66c6e84e4aa671
Ivens de Alvarenga <iveensz_@live.com>
2019-08-28 20:49:09 -0300

    Projeto de métodos de ordenação iniciado: III, IVa e IVb concluídos. I) Os números não podem se repetir II) Pendente III) Concluído IV) Pendente 	a) Concluído desde (2) Métodos de ordenação - 14-8 e 21-8 	b) Concluído 	c) Pendente 	d) Pendente 	e) Pendente V) Pendente 	a) Pendente 	b) Pendente

---
+++ (3) Projeto 1 - Métodos de ordenação - 28-8/metodosOrdenacao.java
@@ -0,0 +1,147 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class metodosOrdenacao {
+
+    public static void main(String[] args) {
+        //Declaração de variável para armazenar o inteiro representando o tamanho do array
+        int n = 0;
+
+        //Declaração do Scanner para leitura dos dados digitados pelo usuário no console
+        Scanner leia = new Scanner(System.in);
+
+        System.out.print("Insira o tamanho (inteiro) do array desejado: ");
+        n = leia.nextInt();
+
+        //Declaração do array utilizando o valor inserido (ou não, logo sendo 0)
+        int[] v = new int[n];
+
+        preencheVetor(v);
+
+        //Começo da apresentação dos dados inseridos no array no output
+        System.out.print("\n\n\t\t\t\t\t[ ");
+
+        //Loop para apresentar os dados populados no array pelo output
+        for (int i = 0; i < v.length; i++) {
+            System.out.print(v[i] + " ");
+        }
+
+        //Fim da apresentação dos dados inseridos no array no output
+        System.out.print("]");
+
+		//Descomente aqui o método de ordenação desejado
+        //bubbleSort(v);
+        //selectionSort(v);
+        //insertionSort(v);
+        //quickSort(v);
+        //Começo da apresentação dos dados inseridos no array no output
+        System.out.print("\n\n\t\t\t\t\t[ ");
+
+        //Loop para apresentar os dados populados no array pelo output
+        for (int i = 0; i < v.length; i++) {
+            System.out.print(v[i] + " ");
+        }
+
+        //Fim da apresentação dos dados inseridos no array no output
+        System.out.print("]");
+    }
+
+    //Implementação do Bubble Sort de acordo com os slides
+    public static void bubbleSort(int vetor[]) {
+        int aux;
+        int tam = vetor.length;
+        for (int i = 0; i < tam - 1; i++) {
+            for (int j = 0; j < tam - 1 - i; j++) {
+                if (vetor[j] > vetor[j + 1]) {
+                    aux = vetor[j];
+                    vetor[j] = vetor[j + 1];
+                    vetor[j + 1] = aux;
+                }
+            }
+        }
+    }
+
+    //Implementação do Selection Sort de acordo com os slides
+    public static void selectionSort(int vetor[]) {
+        int aux;
+        int tam = vetor.length;
+
+        for (int i = 0; i < tam - 1; i++) {
+            for (int j = i + 1; j < tam; j++) {
+                if (vetor[j] < vetor[i]) {
+                    aux = vetor[j];
+                    vetor[j] = vetor[i];
+                    vetor[i] = aux;
+                }
+            }
+        }
+    }
+
+    //Implementação do Insertion Sort de acordo com os slides
+    public static void insertionSort(int vetor[]) {
+        int aux;
+        int tam = vetor.length;
+        int j;
+
+        for (int i = 1; i < tam; i++) {
+            aux = vetor[i];
+            j = i - 1;
+            while (j >= 0 && aux < vetor[j]) {
+                vetor[j + 1] = vetor[j];
+                j--;
+            }
+
+            vetor[j + 1] = aux;
+        }
+    }
+
+    //Implementação do Quick Sort de acordo com os slides
+    public static void quickSort(int vetor[]) {
+        quickSort(vetor, 0, vetor.length - 1);
+    }
+
+    public static void quickSort(int vetor[], int i, int s) {
+        int e = i, d = s;
+        int item = vetor[((e + d) / 2)];
+        while (e <= d) {
+            while (vetor[e] < item) {
+                e++;
+            }
+            while (vetor[d] > item) {
+                d--;
+            }
+            if (e <= d) {
+                int aux; //Variável auxiliar para as trocas
+                aux = vetor[e];
+                vetor[e] = vetor[d];
+                vetor[d] = aux;
+                d--;
+                e++;
+            }
+        }
+
+        if (d - i > 0) {
+            quickSort(vetor, i, d);
+        }
+        if (s - e > 0) {
+            quickSort(vetor, e, s);
+        }
+    }
+
+    //A ideia é ter números diferentes dentro do vetor,
+    //com o range sendo o tamanho do próprio vetor,
+    //e os números sendo aleatórios não repetidos - necessário verificação dentro
+    //do método
+    
+    public static void preencheVetor(int vetor[]){
+        Random random = new Random();
+        int randomArray[] = new int[vetor.length];
+        int i = 0;
+
+        randomArray = random.ints(vetor.length, 0, (vetor.length+1)).toArray();
+        for (int valores : randomArray){
+            vetor[i] = valores;
+            i++;
+        }
+    }
+}
