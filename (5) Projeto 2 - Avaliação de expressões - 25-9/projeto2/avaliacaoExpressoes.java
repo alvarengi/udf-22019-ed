a38879c8c31118a5e72e0347c45a2c4a0e1656f4
Ivens de Alvarenga <iveensz_@live.com>
2019-10-01 20:10:14 -0300

    Projeto de avaliação de expressões: 2º algoritmo proposto concluído

--- (5) Projeto 2 - Avaliação de expressões - 25-9/projeto2/avaliacaoExpressoes.java
+++ (5) Projeto 2 - Avaliação de expressões - 25-9/projeto2/avaliacaoExpressoes.java
@@ -10,19 +10,24 @@ public class avaliacaoExpressoes {
 
 		for(int i = 0; i < expressao.length(); i++){
 			switch(expressao.charAt(i)){
-				case ' ':
-				case '(': 
+				case ' ': 
 				continue;
+				
+				case '(':
+					pilha.push(expressao.charAt(i));
+				break;
 
 				case '+':
 				case '-':
 				case '*':
 				case '/':
-				pilha.push(expressao.charAt(i));
+					if(!pilha.vazia() && (getPrioridadeOperador((char) pilha.top()) >= getPrioridadeOperador(expressao.charAt(i))))
+						System.out.println(pilha.pop());
+					pilha.push(expressao.charAt(i));
 				break;
 
 				case ')':
-				while(!pilha.vazia()) System.out.println(pilha.pop());
+					while(!pilha.vazia() && expressao.charAt(i) != '(') System.out.println(pilha.pop());
 				break;
 
 				default:
@@ -34,5 +39,27 @@ public class avaliacaoExpressoes {
 			System.out.println(pilha.pop());
 
 		return pilha;
+	}
+
+	public static int getPrioridadeOperador(char operando){
+		int prioridade = 0;
+
+		switch(operando) {
+			case '(': 
+				prioridade = 1; 
+				break;
+
+			case '+':
+			case '-':
+				prioridade = 2;
+				break;
+
+			case '*':
+			case '/':
+				prioridade = 3;
+				break;
+		}
+
+		return prioridade;
 	}
 }
--- (5) Projeto 2 - Avaliação de expressões - 25-9/projeto2/testaAvaliacaoExpressoes.java
+++ (5) Projeto 2 - Avaliação de expressões - 25-9/projeto2/testaAvaliacaoExpressoes.java
@@ -6,13 +6,7 @@ public class testaAvaliacaoExpressoes {
 
     public static void main(String[] args) {
 		avaliacaoExpressoes ae = new avaliacaoExpressoes();
-		ae.infixaParaNpr("(A + B) / (C - D)");
-		//ae.infixaParaNpr("(A + B)/(C - D) * E");
-
-		/* Existe um problema no algoritmo apresentado na página 2:
-		   No caso do último passo do algoritmo ainda tem um operando 
-		   dentro da pilha, logo tenho de limpar toda a pilha ao fechar
-		   o parênteses.
-		*/
+		//ae.infixaParaNpr("(A + B) / (C - D)");
+		ae.infixaParaNpr("(7 + 3)/(6 - 4) * 9");
 	}
 }
